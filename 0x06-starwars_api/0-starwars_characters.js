c26826b1c44132d921af7f9395fe2bc90264a94c
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-17 17:49:45 +0300

    Star war episode characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,46 +2,39 @@
 
 const req = require('request');
 
-async function fetchData (url, id) {
-  try {
-    const body = await new Promise((resolve, reject) => {
-      req(url + id, { json: true }, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(body);
-        }
-      });
+function fetchData (url, id) {
+  return new Promise((resolve, reject) => {
+    req(url + id, { json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
     });
-    return body;
-  } catch (error) {
-    console.error(error);
-  }
+  });
 }
 
-async function fetchCharacter (character) {
-  try {
-    const body = await new Promise((resolve, reject) => {
-      req(character, { json: true }, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(body.name);
-        }
-      });
+function fetchCharacter (character) {
+  return new Promise((resolve, reject) => {
+    req(character, { json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body.name);
+      }
     });
-    console.log(body);
-  } catch (error) {
-    console.error(error);
-  }
+  });
 }
 
 fetchData('https://swapi-api.alx-tools.com/api/films/', process.argv[2])
   .then((data) => {
     const characters = data.characters;
-    characters.forEach((character) => {
-      fetchCharacter(character);
-    });
+    return Promise.all(characters.map(fetchCharacter));
+  })
+  .then((characterNames) => {
+    for (const name of characterNames) {
+      console.log(name);
+    }
   })
   .catch((error) => {
     console.error(error);
