ba0b0e5dc6d067e4557377c722c24201d4d9ded5
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-20 22:00:38 +0300

    prime numbers

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-
-'''This module contains a function that determines
-the winner of a game based on prime numbers.'''
+"""Prime game winner determination"""
 
 
 def isWinner(x, nums):
-    def sieve(n):
-        """ Return a list of primes up to n """
-        is_prime = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-                p += 1
-        primes = [p for p in range(2, n + 1) if is_prime[p]]
-        return primes
-
+    """Prime game winner determination"""
     if x < 1 or not nums:
         return None
 
-    max_n = max(nums)
-    primes = sieve(max_n)
+    m_wins = 0
+    b_wins = 0
 
-    maria_wins = 0
+    # generate a list of prime number based on the max numbers in num
+    n = max(nums)
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
 
+    for x in range(2, int(n**0.5) + 1):
+        if primes[x]:
+            for y in range(x**2, n + 1, x):
+                primes[y] = False
+
+    # count the no of pm less than n i nums
     for n in nums:
-        primes_count = len([p for p in primes if p <= n])
-        if primes_count % 2 == 1:
-            maria_wins += 1
+        count = sum(primes[2:n+1])
+        b_wins += count % 2 == 0
+        m_wins += count % 2 == 1
 
-    if maria_wins > x / 2:
-        return "Maria"
-    elif maria_wins < x / 2:
-        return "Ben"
-    else:
+    if m_wins == b_wins:
         return None
+
+    return 'Maria' if m_wins > b_wins else 'Ben'
+
